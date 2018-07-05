37b79ce8e5f2490436bffa1a378d97c48e36dc53
Jerry Hsieh <jerryhsieh01@gmail.com>
2018-07-05 11:37:02 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyApollo msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyApollo from './components/MyApollo.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MyApollo
   }
 }
 </script>
---
+++ src/components/MyApollo.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="my-apollo">
+    <h1>{{ msg }}</h1>
+    <div class="user-info">
+      <table>
+        <thead>
+          <tr>
+            <th>username</th>
+            <th>email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in users" :key="user.id">
+            <td>{{user.username}}</td>
+            <td>{{user.email}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag';
+
+export default {
+  name: 'MyApollo',
+  props: {
+    msg: String
+  },
+  apollo: {
+    users: gql`{
+              users 
+              {
+                id
+                username
+                email
+              }}`
+  },
+  data() {
+    return {
+      users: [] 
+    }
+  }
+}
+</script>
+
+<style scoped>
+.user-info {
+  display: flex;
+  justify-content: center;
+}
+
+table {
+  table-layout: fixed;
+  width: 80%;
+  border: 2px solid grey;
+  border-collapse: collapse;
+  letter-spacing: 1px;
+}
+
+thead th {
+  border: 1px solid grey;
+}
+
+
+tbody {
+  text-align: left;
+}
+
+tbody tr:nth-child(even) {
+  background: #eaeaea;
+}
+
+tbody tr td {
+  border-right: 1px solid grey;
+}
+
+th, td {
+  padding: 5px 10px;
+}
+
+</style>
